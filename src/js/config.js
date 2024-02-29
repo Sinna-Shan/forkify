3bc5682a97d0cb7016c2a5977a5af83b1b621634
Noah Luth <90547744+Sinna-Shan@users.noreply.github.com>
2024-02-29 12:29:30 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1315ea55-72d4-47c2-b35a-6d26c56e1350';
+export const MODAL_CLOSE_SEC = 2.5;
